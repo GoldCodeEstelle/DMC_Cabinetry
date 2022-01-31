53a87cfaa4b03ce3e896ca4ade4355ceb9dda62b
Kestelle <Estelle12344@gmail.com>
2022-01-31 11:40:46 -0600

    initial commit

--- python_project/settings.py
+++ python_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-jpq@)tit@qdqg8vx1!b8yk@*sy(#fu*v&n-%+d)*o8b)ytrai4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'dmc-cabinetry.herokuapp.com']
 
